3398dc54ffe49d7822db8e1f51be4306d570f396
Sting Alleman <stingalleman@icloud.com>
2021-09-23 22:09:56 +0100

    test

--- setup-builder/setup.sh
+++ setup-builder/setup.sh
@@ -1,5 +1,15 @@
-#!/bin/bash
+#! /usr/bin/env bash
 
-echo "hi!"
-ls
-pwd
+arch=$(uname -m)
+
+if [ "$arch" == "arm64" ]
+then
+    arch="silicon"
+elif [ "$arch" == "x86_64" ]
+then
+    arch="intel"
+fi
+
+echo "$PATH"
+
+# # wget "https://core.pakket.sh/pakket-builder/$arch/pakket-builder" -O /usr/local/bin/pakket-builder
