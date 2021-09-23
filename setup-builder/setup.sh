8284bfe334a5c259e2827e68df4e5e76169fb48c
Sting Alleman <stingalleman@icloud.com>
2021-09-23 23:13:03 +0100

    a

--- setup-builder/setup.sh
+++ setup-builder/setup.sh
@@ -16,6 +16,4 @@ echo "::add-path::$HOME/.local/bin"
 wget "https://core.pakket.sh/pakket-builder/$arch/pakket-builder" -q -O "$HOME/.local/bin/pakket-builder"
 chmod +x "$HOME/.local/bin/pakket-builder"
 
-ls "$HOME/.local/bin"
-~/.local/bin/pakket-builder -h
 pakket-builder -h
